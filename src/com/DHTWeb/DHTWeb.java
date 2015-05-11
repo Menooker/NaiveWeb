75a9c2fb7f9d9da2ea3177cc39a7c5d7ea647573
Menooker <myjisgreat@live.cn>
2015-05-11 21:14:19 +0800

    adapt to tomp2p 5.0. now it's working on linux!

--- src/com/DHTWeb/DHTWeb.java
+++ src/com/DHTWeb/DHTWeb.java
@@ -2,35 +2,80 @@ package com.DHTWeb;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.util.Collection;
 
-import net.tomp2p.futures.FutureDHT;
+import net.tomp2p.connection.Bindings;
+import net.tomp2p.connection.DiscoverNetworks;
+import net.tomp2p.dht.FutureGet;
+import net.tomp2p.dht.PeerBuilderDHT;
+import net.tomp2p.dht.PeerDHT;
 import net.tomp2p.futures.FutureBootstrap; 
 import net.tomp2p.futures.FutureDiscover;
+import net.tomp2p.nat.FutureNAT;
+import net.tomp2p.nat.FutureRelayNAT;
+import net.tomp2p.nat.PeerBuilderNAT;
+import net.tomp2p.nat.PeerNAT;
 import net.tomp2p.p2p.Peer;
-import net.tomp2p.p2p.PeerMaker;
+import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.PeerAddress;
+import net.tomp2p.relay.tcp.TCPRelayClientConfig;
 import net.tomp2p.storage.Data;
 
 public class DHTWeb {
 
-	static private Peer peer;
+	static private PeerDHT peer;
 
     public DHTWeb(int peerId) throws Exception {
-        peer = new PeerMaker(Number160.createHash(peerId)).setPorts(4000 ).makeAndListen();
-        FutureBootstrap fb = peer.bootstrap().setBroadcast().setPorts(4000).start();
+    	peer = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(peerId)).ports(4000 ).start()).start();
+        FutureBootstrap fb = peer.peer().bootstrap().inetAddress(InetAddress.getByName("127.0.0.1")).ports(4001).start();
         fb.awaitUninterruptibly();
-        if (fb.getBootstrapTo() != null) {
-            peer.discover().setPeerAddress(fb.getBootstrapTo().iterator().next()).start().awaitUninterruptibly();
+    	System.out.println("KKKK");
+        if (fb.isSuccess()) {
+            peer.peer().discover().peerAddress(fb.bootstrapTo().iterator().next()).start().awaitUninterruptibly();
         }
     }
     public DHTWeb(String host,int peerId) throws Exception {
-    	peer = new PeerMaker(Number160.createHash(peerId)).setPorts(4000 + peerId).makeAndListen();
-    	InetAddress address = Inet4Address.getByName(host);
-    	FutureDiscover futureDiscover = peer.discover().setInetAddress( address ).setPorts( 4000 ).start();
-    	futureDiscover.awaitUninterruptibly();
-    	System.out.println(futureDiscover.isSuccess());
-    	FutureBootstrap futureBootstrap = peer.bootstrap().setInetAddress( address ).setPorts( 4000 ).start();
-    	futureBootstrap.awaitUninterruptibly();
+    	Bindings b = new Bindings().listenAny();
+    	peer= new PeerBuilderDHT(new PeerBuilder(new Number160(peerId)).ports(4000+peerId).bindings(b).behindFirewall().start()).start();
+		System.out.println("Client started and Listening to: " + DiscoverNetworks.discoverInterfaces(b));
+		System.out.println("address visible to outside is " + peer.peerAddress());
+
+		InetAddress address = Inet4Address.getByName(host);
+		int masterPort = 4000;
+		PeerAddress pa = new PeerAddress(Number160.ZERO, address, masterPort, masterPort);
+
+		System.out.println("PeerAddress: " + pa);
+		
+		PeerNAT peerNAT = new PeerBuilderNAT(peer.peer()).start();
+		FutureDiscover fd = peer.peer().discover().peerAddress(pa).start();
+		FutureNAT fn = peerNAT.startSetupPortforwarding(fd);
+		FutureRelayNAT frn = peerNAT.startRelay(new TCPRelayClientConfig(), fd, fn);
+		
+		frn.awaitUninterruptibly();
+		if (fd.isSuccess()) {
+			System.out.println("found that my outside address is " + fd.peerAddress());
+		} else {
+			System.out.println("dis failed " + fd.failedReason());
+		}
+		
+		if (fn.isSuccess()) {
+			System.out.println("NAT success: " + fn.peerAddress());
+		} else {
+			System.out.println("nat failed " + fn.failedReason());
+		}
+		
+		if (frn.isSuccess()) {
+			System.out.println("FutureRelay success");
+		} else {
+			System.out.println("relay failed " + frn.failedReason());
+		}
+		
+		// Future Bootstrap - slave
+		FutureBootstrap futureBootstrap = peer.peer().bootstrap().inetAddress(address).ports(masterPort).start();
+		futureBootstrap.awaitUninterruptibly();
+		
+		
     }
     public static void main(String[] args) throws NumberFormatException, Exception {
     	DHTWeb dns;
@@ -45,18 +90,16 @@ public class DHTWeb {
         }
     }
 
-    private String get(String name) throws ClassNotFoundException, IOException {
-        FutureDHT futureDHT = peer.get(Number160.createHash(name)).start();
-        futureDHT.awaitUninterruptibly();
-        if (futureDHT.isSuccess()) {
-            return futureDHT.getData().getObject().toString();
-        }
-        System.out.println(futureDHT.getFailedReason());
-        return "not found";
-    }
-
+	private String get(String name) throws ClassNotFoundException, IOException {
+		FutureGet futureGet = peer.get(Number160.createHash(name)).start();
+		futureGet.awaitUninterruptibly();
+		if (futureGet.isSuccess()) {
+			return futureGet.dataMap().values().iterator().next().object().toString();
+		}
+		return "not found";
+	}
     private void store(String name, String ip) throws IOException {
-        peer.put(Number160.createHash(name)).setData(new Data(ip)).start().awaitUninterruptibly();
+        peer.put(Number160.createHash(name)).data(new Data(ip)).start().awaitUninterruptibly();
     }
 
 }
